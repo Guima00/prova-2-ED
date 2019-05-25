f7498d11354dfe5a755ad01e74d06c80dae9988a
Guima <48738309+Guima00@users.noreply.github.com>
2019-05-25 17:05:58 -0300

    Add files via upload

---
+++ MatrizEsparsa.cpp
@@ -0,0 +1,71 @@
+#include <cstdlib>
+#include <iostream>
+#include "MatrizEsparsa.h"
+
+using namespace std;
+
+MatrizEsparsa::MatrizEsparsa(int d)
+{
+    dim = d;
+    nmax = dim * dim / 4;
+    n = 0;
+    linha = new int[nmax];
+    coluna = new int[nmax];
+    vet = new int[nmax];
+}
+
+MatrizEsparsa::~MatrizEsparsa()
+{
+    delete [] linha;
+    delete [] coluna;
+    delete [] vet;
+}
+
+// ----------------------------------------------------------------------------
+//Q4
+
+void MatrizEsparsa::insere(int lin, int col, int val)
+{
+    /// Implemente aqui sua solução.
+}
+
+//-Q4
+// ----------------------------------------------------------------------------
+
+int MatrizEsparsa::busca(int lin, int col)
+{
+    int i = 0;
+
+    while(i < n)
+    {
+        if(lin == linha[i] && col == coluna[i])
+            return -1;
+
+        if(lin < linha[i] || (lin == coluna[i] && col < coluna[i]))
+            break;
+
+        i++;
+    }
+
+    return i;
+}
+
+void MatrizEsparsa::imprime()
+{
+    int k = 0;
+    for(int i = 0; i < dim; i++)
+    {
+        for(int j = 0; j < dim; j++)
+        {
+            if(i == linha[k] && j == coluna[k])
+            {
+                cout << vet[k] << '\t';
+                k++;
+            }
+            else
+                cout << 0 << '\t';
+        }
+        cout << endl;
+    }
+    cout << endl;
+}
